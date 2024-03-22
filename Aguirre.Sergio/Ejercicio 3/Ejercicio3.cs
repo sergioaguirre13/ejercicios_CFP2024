c9afc03bc34af09193b72929f83a89c1ab65c28d
sergioaguirre13 <sergio.aguirre@bue.edu.ar>
2024-03-22 19:21:22 -0300

    Tercer ejercicio

---
+++ Aguirre.Sergio/Ejercicio 3/Ejercicio3.cs
@@ -0,0 +1,24 @@
+﻿namespace Ejercicio_3
+{
+    internal class Ejercicio3
+    {
+        static void Main(string[] args)
+        {
+            // 3.Ingresar 2 números y mostrar la suma de los mismos.
+
+            int num1;
+            int num2;
+            int totalSuma;
+
+            Console.WriteLine("Por favor ingrese 2 números para sumar");
+            Console.WriteLine("Primer número");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Segundo número");
+            num2 = int.Parse(Console.ReadLine());
+
+            totalSuma = num1 + num2;
+
+            Console.WriteLine($"El resultado de la suma de {num1} y {num2} es {totalSuma}");
+        }
+    }
+}
--- Aguirre.Sergio/Ejercicio 3/obj/Debug/net8.0/Ejercicio 3.AssemblyInfo.cs
+++ Aguirre.Sergio/Ejercicio 3/obj/Debug/net8.0/Ejercicio 3.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Ejercicio 2")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Ejercicio 3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Ejercicio 2")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Ejercicio 2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+699ea3c1980c51b3186ad8792a4670633500b749")]
+[assembly: System.Reflection.AssemblyProductAttribute("Ejercicio 3")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Ejercicio 3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
