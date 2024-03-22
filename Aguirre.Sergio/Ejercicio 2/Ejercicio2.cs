699ea3c1980c51b3186ad8792a4670633500b749
sergioaguirre13 <sergio.aguirre@bue.edu.ar>
2024-03-22 14:42:47 -0300

    Segundo ejercicio

--- Aguirre.Sergio/Ejercicio 2/Ejercicio2.cs
+++ Aguirre.Sergio/Ejercicio 2/Ejercicio2.cs
@@ -1,23 +1,26 @@
-﻿using System;
-
-namespace Ejercicio_1
+﻿namespace Ejercicio_2
 {
-    internal class Ejercicio1
+    internal class Ejercicio2
     {
         static void Main(string[] args)
         {
-
-            // 1. Ingresar el nombre y apellido por consola.
+            // 2.Ingresar el nombre, apellido y edad por consola
 
             string nombre;
             string apellido;
+            int edad;
+            
 
             Console.WriteLine("Por favor ingrese su nombre: ");
             nombre = Console.ReadLine();
             Console.WriteLine("Por favor ingrese su apellido: ");
             apellido = Console.ReadLine();
+            Console.WriteLine("Por favor ingrese su edad: ");
+            edad = int.Parse(Console.ReadLine());
+            
 
             Console.WriteLine($"Su nombre es {nombre} y su apellido es {apellido}");
+            Console.WriteLine($"Su edad es: {edad} años");
         }
     }
 }
--- Aguirre.Sergio/Ejercicio 2/obj/Debug/net8.0/Ejercicio 2.AssemblyInfo.cs
+++ Aguirre.Sergio/Ejercicio 2/obj/Debug/net8.0/Ejercicio 2.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("RepasoCondicionales")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Ejercicio 2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("RepasoCondicionales")]
-[assembly: System.Reflection.AssemblyTitleAttribute("RepasoCondicionales")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e395ea49c002d63ee7ffb30e5878c039db8abfc5")]
+[assembly: System.Reflection.AssemblyProductAttribute("Ejercicio 2")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Ejercicio 2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
