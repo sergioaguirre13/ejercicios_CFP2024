67621c31edc6a0decb0367a8987ec24b77fac254
sergioaguirre13 <sergio.aguirre@bue.edu.ar>
2024-03-29 08:37:49 -0300

    Strings EJ 4

---
+++ Aguirre.Sergio/StringEjercicio4/StringsEjercicio4.cs
@@ -0,0 +1,24 @@
+﻿namespace StringsEjercicio4
+{
+    internal class StringsEjercicio4
+    {
+        static void Main(string[] args)
+        {
+            //            4.Del string 0000001234500000000 conseguir quedarse con el string 12345 y
+            //mostrarlo por pantalla.
+
+            string numeros = "0000001234500000000";
+            string numeroSinCeros = "";
+
+            foreach (char numero in numeros)
+            {
+                if (numero != '0')
+                {
+                    numeroSinCeros += numero;
+                }
+            }
+
+            Console.WriteLine(numeroSinCeros);
+        }
+    }
+}
