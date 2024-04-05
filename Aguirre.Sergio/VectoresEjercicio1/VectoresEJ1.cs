6c9d859429cb967a617944cf6bf9449ca49e119f
sergioaguirre13 <sergio.aguirre@bue.edu.ar>
2024-04-05 12:16:18 -0300

    Vector EJ 1

---
+++ Aguirre.Sergio/VectoresEjercicio1/VectoresEJ1.cs
@@ -0,0 +1,22 @@
+﻿using System.Numerics;
+
+namespace VectoresEjercicio1
+{
+    internal class VectoresEJ1
+    {
+        static void Main(string[] args)
+        {
+
+            //Cargar un vector de enteros de 5 elementos y mostrarlo.
+
+            int[] numeros = { 1, 2, 3, 4, 5 };
+
+            foreach (int numero in numeros)
+            {
+                Console.WriteLine(numero);
+            }
+
+
+        }
+    }
+}
