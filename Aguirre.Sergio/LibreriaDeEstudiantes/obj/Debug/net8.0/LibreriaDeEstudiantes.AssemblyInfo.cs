24eb10190afe199a03209579e01fc612bad4a882
sergioaguirre13 <sergio.aguirre@bue.edu.ar>
2024-05-05 23:29:54 -0300

    ejercicio CLASS 05/05

--- Aguirre.Sergio/LibreriaDeEstudiantes/obj/Debug/net8.0/LibreriaDeEstudiantes.AssemblyInfo.cs
+++ Aguirre.Sergio/LibreriaDeEstudiantes/obj/Debug/net8.0/LibreriaDeEstudiantes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibreriaDeEstudiantes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74bfebceeabe0fcf80e5d59e625aa4b09644b8c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibreriaDeEstudiantes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibreriaDeEstudiantes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
